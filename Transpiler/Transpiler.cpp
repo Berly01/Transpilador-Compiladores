247095ba0005c4fb08393ce646a11b70823bc270
JoelDiaz0 <95628128+JoelDiaz0@users.noreply.github.com>
2025-07-23 11:55:18 -0500

    add

--- Transpiler/Transpiler.cpp
+++ Transpiler/Transpiler.cpp
@@ -1,34 +1,42 @@
 ﻿#include <iostream>
+#include <fstream>
 #include "PrintfToCoutTranspiler.hpp"
 #include "DefineTranspiler.hpp"
 #include "NullTranspiler.hpp"
 #include "ArrayTranspiler.hpp"
 #include "StringTranspiler.hpp"
 
+std::string test_input();
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
 
-    std::string input;
-    std::string output;
+    std::string input_file;
+    std::string output_ile;
+    std::string content;
 
     if (argc == 3) {
-        input = argv[1];
-        output = argv[2];
+        input_file = argv[1];
+        output_ile = argv[2];
+
+        std::ifstream inFile(input_file);
+        if (!inFile.is_open()) {
+            std::cerr << "No se pudo abrir el archivo: " << input_file << "\n";
+            return 1;
+        }
+
+        std::string aux_content((std::istreambuf_iterator<char>(inFile)),
+            std::istreambuf_iterator<char>());
+        inFile.close();
+
+        content = aux_content;
     }
     else {
-        return 1;
+        std::cerr << "No se ingreso un archivo de entrada y salida, se utilizara un archivo de prueba\n";
+        content = test_input();
+        output_ile = "test-output.cpp";
     }
 
-    std::ifstream inFile(input);
-    if (!inFile.is_open()) {
-        exit(1);
-    }
-    std::string content((std::istreambuf_iterator<char>(inFile)),
-        std::istreambuf_iterator<char>());
-    inFile.close();
-
-    std::ofstream outFile(output, std::ofstream::trunc);
+    std::ofstream outFile(output_ile, std::ofstream::trunc);
 
     try {
         DefineTranspiler defineTranspiler;
@@ -52,6 +60,43 @@ int main(int argc, char** argv)
         return 1;
     }
 
+	return 0;
+}
+
+std::string test_input() {
+
+    std::string test_input = R"(#include <stdio.h>
+
+#define PI 3.1416
+
+int main() {
+
+	char* firstname = "mike";
+
+	char lastname[] = "perez";
+
+	int money = 2000;
+
+	double radio = PI * 100;
+
+	char c[] = { 'a', 'b', 'c', 'd', 'e' };
+
+	double nums[] = { 1.2, 1.3, 1.4, 1.5, 1.6 };
+
+	int i[10];
+
+	int* x = (int*)malloc(sizeof(int) * 10);
+
+	printf("hola %s %s, tienes %d soles?", firstname, lastname, money);
+
+	int* ptr = NULL;
 
 	return 0;
+})";
+
+    std::ofstream out("test_input.c", std::ostream::trunc);
+    out << test_input;
+    out.close();
+
+    return test_input;
 }
